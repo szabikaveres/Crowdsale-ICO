2d5bfa7c121696fafd22f2ce81277259114a8cd1
Szabolcs Veres <szabolcsveres2022@gmail.com>
2023-12-19 10:59:59 +0000

    Buy Tokens function

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,7 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract Crowdsale {
-    string public name = "Crowdsale";
+    Token public token; //Token is a smart contract type
+    uint public price;
+    uint public maxTokens;
+    uint public tokensSold;
+    
+    event Buy(uint amount, address buyer);
+
+    //Need address
+    constructor(
+        Token _token, 
+        uint _price,
+        uint _maxTokens
+    ) {
+        token = _token;
+        price = _price; //price = 1 Token / 1 Ether
+        maxTokens = _maxTokens;
+    }
+
+    function buyTokens(uint _amount) public payable { // payable tells solidity that we send ether in with that transaction 
+        require (msg.value ==( _amount / 1e18 ) *  price);//msg.value = the amount of ether being sent in, 1e18 = 10 ** 18
+        //above we are making sure, they are sending in enough crypt to setisfy the following condition
+       require(token.balanceOf(address(this)) >= _amount);//this corresponds to the address of the current smart contract, that we are writing code inside of
+       require (token.transfer(msg.sender, _amount));//transfer is referencing to the transfer function from Token.sol
+        //by addig require, telling solidity to make ssure that  condition happends before attempting to do anything else
+    
+        tokensSold += _amount;
+
+        emit Buy(_amount, msg.sender);
+    }
 
 }
--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -4,18 +4,86 @@ const { ethers } = require('hardhat');
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
+const ether = tokens
 
 describe ('Crowdsale', () => {
-    let crowdsale
+    let crowdsale, token, accounts, user1
 
     beforeEach(async () => {
-        const Crowdsale  = await ethers.getContractFactory("Crowdsale")
-        crowdsale = await Crowdsale.deploy()
+        //load contracts
+        const Crowdsale  = await ethers.getContractFactory("Crowdsale") //deploy the contract
+        const Token = await ethers.getContractFactory('Token') //deploy the token
+
+        //deploy token
+        token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+
+        //configure accounts
+        accounts = await ethers.getSigners()
+        deployer = accounts[0] //user who transfers tokens
+        user1 = accounts[1] //buys tokens
+
+        //deploy crowdsale, and add the price for swap 1:1
+        crowdsale = await Crowdsale.deploy(token.address, ether(1), '1000000')
+
+        //send tokens to Crowdsale
+        let transaction = await token.connect(deployer).transfer(crowdsale.address, tokens(1000000))
+        await transaction.wait()
     })
     
     describe ('Deployment', () => {
-        it('has correct name',async () => {
-            expect(await crowdsale.name()).to.equal("Crowdsale")
+
+        it('sends  tokens to the Crowdsale contract ',async () => {
+            expect(await token.balanceOf(crowdsale.address)).to.equal(tokens(1000000))
         })
+
+        it('returns the price',async () => {
+            expect(await crowdsale.price()).to.equal(ether(1))
+        })
+
+        it('return token address',async () => {
+            expect(await crowdsale.token()).to.equal(token.address)
+        })
+
     })
+
+    describe ('Buying tokens', () => {
+        let transaction, result    
+        let amount = tokens(10)
+
+        describe ('Success', () => {
+
+            beforeEach(async () => {
+                transaction = await crowdsale.connect(user1).buyTokens(amount,{ value:ether(10) })
+                result = await transaction.wait()
+            })
+
+            it('transfers tokens',async () => {
+                expect(await token.balanceOf(crowdsale.address)).to.equal(tokens(999990))
+                expect(await token.balanceOf(user1.address)).to.equal(amount)
+            })
+
+            it('updates tokenSold ',async () => {
+                expect(await crowdsale.tokensSold()).to.equal(amount)
+            })
+            
+            it('updates contracts ether balance',async () => {
+                expect(await ethers.provider.getBalance(crowdsale.address)).to.equal(amount)
+            })
+
+            it('emits a buy event',async () => {
+                // --> https://hardhat.org/hardhat-chai-matchers/docs/reference#.emit
+                await expect(transaction).to.emit(crowdsale, 'Buy').withArgs(amount, user1.address)
+            })
+            
+        })
+
+        describe ('Failure', () => {
+
+            it('rejects insufficient ETH',async () => {
+                await expect(crowdsale.connect(user1).buyTokens(tokens(10), {value: 0})).to.be.reverted
+            })
+        })
+
+    })
+
 })
